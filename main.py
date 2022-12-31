d5ff25d5b06c3d72cc62457258640ea32a769d57
juhc <chalkov2002@mail.ru>
2022-12-31 18:42:55 +0500

    final

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='192.168.0.104', use_reloader=True)
+    socketio.run(app, use_reloader=True)
